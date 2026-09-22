c619d1072d22ead9f83961c62e5a6062347d597f
Magnus <magnuspaulsen2002@gmail.com>
2026-09-22 11:06:43 +0200

    Renamed env path

--- frontend/src/api/apiclient.ts
+++ frontend/src/api/apiclient.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const api_url = import.meta.env.VITE_API_URL
+const api_url = import.meta.env.VITE_BACKEND_API_PATH
 
 export const apiclient = axios.create({baseURL: api_url})
